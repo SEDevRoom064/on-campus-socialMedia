e82eb44a8a03cc8a407d27b41481d3a9b44052b3
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-06-21 18:46:32 +0100

    Update swagger.js

--- BACKEND/swagger.js
+++ BACKEND/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:5000", // matches the dev URL
+        url: "http://localhost:3000", // matches the dev URL
       },
     ],
   },
